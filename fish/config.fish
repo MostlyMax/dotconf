fca82ee7d6b11821f97ae5a9bfb2d9610f1ac7e9
Max Saparov <max.saparov@acronis.com>
2023-05-13 17:51:31 -0400

    inital commit

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+
+function fish_greeting
+    fastfetch
+end
+
+alias ls="exa -lha"
+alias cat=bat
+alias top=btm
+alias please=sudo
+alias ff=fastfetch
+
+fish_add_path /usr/local/bin
+fish_add_path /Users/max/.cargo/bin
+
